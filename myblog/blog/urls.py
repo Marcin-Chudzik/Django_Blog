cd08c85bcb3c8268fee5de215b074e1d03c12505
Marcin_Chudzik <marcinchudzik.pythondev@gmail.com>
2023-03-19 12:39:13 +0100

    Refactor files with small amount of code, mostly estetic.

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -5,7 +5,6 @@ from .feeds import LatestPostsFeed
 app_name = 'blog'
 
 urlpatterns = [
-    # Post views
     path('', views.post_list, name='post_list'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
